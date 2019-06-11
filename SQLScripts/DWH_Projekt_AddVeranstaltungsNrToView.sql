0f8b1afc654d840a6bc3d23989a6b34e410512e0
richy <richardputz@gmx.at>
2019-06-11 14:52:07 +0200

    add packages for etl

---
+++ SQLScripts/DWH_Projekt_AddVeranstaltungsNrToView.sql
@@ -0,0 +1,19 @@
+/****** Object:  View [dbo].[v_Veranstaltungen]    Script Date: 11.06.2019 13:50:42 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER VIEW [dbo].[v_Veranstaltungen]
+AS
+SELECT        dbo.Studienrichtungen.Studienrichtung, dbo.Studiengaenge.Studiengangsname, dbo.Veranstaltungsarten.Veranstaltungsart, dbo.Veranstaltungen.Veranstaltungsname,dbo.Veranstaltungen.VerantstaltungsNr as VeranstaltungsBusinessKey
+FROM            dbo.Studiengaenge INNER JOIN
+                         dbo.Studienrichtungen ON dbo.Studiengaenge.StudienrichtungNrFK = dbo.Studienrichtungen.StudienrichtungNr INNER JOIN
+                         dbo.Veranstaltungen ON dbo.Studiengaenge.StudiengangsNr = dbo.Veranstaltungen.StudiengangNrFK INNER JOIN
+                         dbo.Veranstaltungsarten ON dbo.Veranstaltungen.VeranstaltungsArtNrFK = dbo.Veranstaltungsarten.VerantstaltungsArtNr
+
+GO
+
+
---
+++ SQLScripts/DWH_Projekt_StagingDBCreation.sql
@@ -0,0 +1,54 @@
+CREATE TABLE [dbo].[Personen_Denormalized](
+	[PersonBusinessKey] [int] NOT NULL primary key,
+	[Vorname] [nvarchar](50) NULL,
+	[Nachname] [nvarchar](50) NULL,
+	[Geschlecht] [nvarchar](10) NULL,
+	[Geburtsdatum][date] NULL,
+	[Art] [nvarchar](50) NULL,
+)
+
+CREATE TABLE [dbo].[Personen_Insert](
+	[PersonBusinessKey] [int] NOT NULL primary key,
+	[Vorname] [nvarchar](50) NULL,
+	[Nachname] [nvarchar](50) NULL,
+	[Geschlecht] [nvarchar](10) NULL,
+	[Geburtsdatum][date] NULL,
+	[Art] [nvarchar](50) NULL,
+)
+
+CREATE TABLE [dbo].[Personen_Update](
+	[PersonBusinessKey] [int] NOT NULL primary key,
+	[Vorname] [nvarchar](50) NULL,
+	[Nachname] [nvarchar](50) NULL,
+	[Geschlecht] [nvarchar](10) NULL,
+	[Geburtsdatum][date] NULL,
+	[Art] [nvarchar](50) NULL,
+)
+
+CREATE TABLE [dbo].[Person_Delete](
+	[PersonBusinessKey] [int] NOT NULL primary key,
+	
+)
+CREATE TABLE [dbo].[Veranstaltungen](
+	[VerantstaltungsBusinessKey]  [int] NOT NULL primary key,
+	[Veranstaltungsname] [nvarchar](50) NULL,
+	[WS] [decimal](10, 2) NULL,
+	[Studiengangsname] [nvarchar](50) NULL,
+	[Studienrichtung] [nvarchar](50) NULL,
+	[Veranstaltungsart] [nvarchar](50) NULL,
+)
+	CREATE TABLE [dbo].[Studienrichtungen](
+	[StudienrichtungsBusinessKey] [int] NOT NULL primary key,
+	[Name] [nvarchar](50) NULL,
+)
+CREATE TABLE [dbo].[Evaluierungen](
+	[EvaluierungBusinessKey] [int] NOT NULL primary key,
+	[VeranstaltungsBusinessKey] [int] NOT NULL,
+	[StudienrichtungBusinessKey] [int] NOT NULL,
+	[PersonBusinessKey] [int] NOT NULL,
+	[Datum] [date] NULL,
+	[AufbauUndGliederungDerLerhveranstaltung] [int] NULL,
+	[LehrveranstaltungsleitungKompetenz] [int] NULL,
+	[LehrveranstaltungsleitungEingehenAufFragen] [int] NULL,
+	[BesondersGut] [nvarchar](50) NULL,
+)
